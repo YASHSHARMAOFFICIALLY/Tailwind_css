8a307ed17afc44144f7137162b1d0c252855fe0a
Yash sharma <yashsharmaofficially@gmail.com>
2025-12-30 18:17:16 +0530

    hero

--- hero_section/app/page.tsx
+++ hero_section/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import { Hero } from "@/component/hero";
 export default function home() {
   return (
-    <div className=" justify-center flex bg-red-500">
-      hi there
+    <div className=" h-screen bg-neutral-900 flex justify-center">
+      <Hero/>
     </div>
   )
 }
---
+++ hero_section/component/hero.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+export const Hero=()=>{
+    return(
+        <div className="my-40 w-full flex flex-col items-center">
+            <h1 className="bg-gradient-to-b from-neutral-50 to-neutral-500 bg-clip-text text-transparent w-fit text-7xl font-bold max-w-2xl text-center tracking-tight leading-tight">
+                             Unleash the power of intuitive finance
+                                </h1>
+            <p className="text-neutral-300 text-center mt-10 max-w-3xl text-xl selection:bg-white">
+                say goodbye to the outdated financial tools.Every small business owner,
+                regardless of the background,can now manage their business like a pro 
+                simple intuitive And never boring 
+            </p>
+            <div className="flex justify-center mt-8 w-full max-w-lg">
+                <input type="text"
+                 className=" flex-1 border border-neutral-600 mr-4 rounded-xl px-4 py-2 placeholder:text-neutral-500 text-white 
+                 focus:outline-none focus:ring-2 focus:ring-sky-500 transition duration-200"
+                 placeholder="Enter your email"  />
+                <button className="relative px-4 py-2 rounded-2xl border border-neutral-700 cursor-pointer text-white">
+                    <div className="absolute inset-x-0 -bottom-px h-px w-full bg-gradient-to-r from-transparent via-sky-600 to-transparent"></div>
+                    Join Waitlist</button>
+            </div>
+        </div>
+    )
+}
